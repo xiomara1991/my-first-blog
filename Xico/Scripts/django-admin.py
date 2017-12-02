01f1bfbb1cfd83619233c0210c4e3d4288f5d95f
xiomara <xiomaravascozambrano@gmail.com>
2017-12-02 17:29:27 -0500

    My Django Girls app, first commit

---
+++ Xico/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\golden\desktop\django explicacion\djangogirls\xico\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
